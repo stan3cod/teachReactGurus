5d5103506b5bb14577e8dec0e1617e7a359bb956
stan3cod <kodstan33@gmail,com>
2023-07-28 13:45:24 +0100

    first commit

---
+++ pages/admin/adminDashboard.js
@@ -0,0 +1,7 @@
+function AdminDashboard(){
+    return(
+        <h1>THIS IS ADMIN FOLDER</h1>
+    )
+}
+
+export default AdminDashboard
---
+++ pages/index.js
@@ -0,0 +1,34 @@
+// import classes from './index.module.css'
+
+
+
+
+// function WitNDlight(){
+//   return(
+//     <div className={classes.picture}><div><img src='https://media.witanddelight.com/content/uploads/2023/06/21132752/Nine-Pines-Bathroom.jpg'></img></div>
+//     <div className={classes.Writeup}>
+//       <h1>INTERIORS & DECOR</h1>
+//       <a href='#'>Introducing My First
+//       <br></br> Interior Design Project: <br></br>
+//       A 1980s House Full of<br></br> Ornate Details</a><br></br>
+//       <p>
+//         I've been designing in a more hands-on capacity outside of my own <br></br>
+//         home.I'm excited to introduce my first interior design project.
+//       </p>
+//       <hr></hr>
+//       <h2>READ POST</h2>
+
+//     </div>
+    
+    
+//     </div>
+  
+//   )
+  
+// }
+// export default WitNDlight
+// import BannerSection from '@/component/banner/bannerSection'
+// import classes from '.index.module.css'
+// function Home(){
+
+// }
